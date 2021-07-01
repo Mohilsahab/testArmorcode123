b998fb3d1a40a774092b9c34732fbd85cc1d59ff
Mohilsahab <vinodmohil@gmail.com>
2021-07-01 08:18:00 +0530

    testing the file in testBranch

--- src/main/java/src/loadBalancer/Client.java
+++ src/main/java/src/loadBalancer/Client.java
@@ -1,2 +1,12 @@
-package src.loadBalancer;\n\n
-public class Client {\n    public static void main(String[] args) throws Exception{\n        LoadBalancer loadBalancer = new LoadBalancer(2, 3);\n        int id = loadBalancer.executeTasks(50, 50);\n        int id2 = loadBalancer.executeTasks(30, 40);\n        int id3 = loadBalancer.executeTasks(30, 40);\n        int id4 = loadBalancer.executeTasks(30, 40);;\n    }\n}
+package src.loadBalancer;
+
+public class Client {
+    public static void main(String[] args) throws Exception{
+        LoadBalancer loadBalancer = new LoadBalancer(2, 3);
+        int id = loadBalancer.executeTasks(50, 50);
+        int id2 = loadBalancer.executeTasks(30, 40);
+        int id3 = loadBalancer.executeTasks(30, 40);
+        int id4 = loadBalancer.executeTasks(30, 40);
+        System.out.println("test");
+    }
+}
